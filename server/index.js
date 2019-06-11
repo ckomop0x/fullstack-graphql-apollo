774f3266f9a085384ba0b12a514330abbb0b67e5
Pavel Klochkov <paul.klochkov@gmail.com>
2019-06-11 15:53:51 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const {ApolloServer} = require('apollo-server')
+
+// Server requires 2 fields
+// GraphQL schema
+const typeDefs = `
+  type Query {
+    "Count of total days skied during a season"
+    totalDays: Int
+  }
+`
+
+// Functions that resolve data for each field
+const resolvers = {
+  Query: {
+    totalDays: () => 100
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server
+  .listen()
+  .then(({url}) => `GraphQL server is listening on ${url}`)
+  .then(console.log)
+  .catch(console.error)
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const endpoint = 'http://localhost:4000'
+const fetch = require('isomorphic-unfetch')
+
+const query = `
+  query {
+    totalDays
+  }
+`
+
+const opts = {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({query})
+}
+
+fetch(endpoint, opts)
+  .then(res => res.json())
+  .then(({ data }) => `totalDays: ${data.totalDays} - it's cool`)
+  .then(console.log)
+  .catch(console.error)
